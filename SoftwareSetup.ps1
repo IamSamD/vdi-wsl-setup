a93d24fc606f110f70a6be639fd2048881a35b32
Sam Dickinson <sam.dickinson@nhg.org.uk>
2024-11-05 17:00:05 +0000

    fix: detect software already installed before attempting to install

--- SoftwareSetup.ps1
+++ SoftwareSetup.ps1
@@ -1,40 +1,48 @@
 $ErrorActionPreference = 'Stop'
 
 # Install tfenv
-Write-Host "Installing tfenv..." -ForegroundColor Cyan
-$output = git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Error cloning tfenv: $Output"
-    $LASTEXITCODE = 0
-    exit 1
-}
-Write-Host "tfenv cloned successfully`n" -ForegroundColor Green
+if (-net(Test-Path "~/.tfenv")) {
+    Write-Host "Installing tfenv..." -ForegroundColor Cyan
+    $output = git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error cloning tfenv: $Output"
+        $LASTEXITCODE = 0
+        exit 1
+    }
+    Write-Host "tfenv cloned successfully`n" -ForegroundColor Green
 
-Write-Host "Adding tfenv to path..." -ForegroundColor Cyan
-$Output = echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Error writing to .bashrc: $Output"
-    $LASTEXITCODE = 0
-    exit 1
+    Write-Host "Adding tfenv to path..." -ForegroundColor Cyan
+    $Output = echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error writing to .bashrc: $Output"
+        $LASTEXITCODE = 0
+        exit 1
+    }
+    Write-Host "tfenv installed and configured successfully`n" -ForegroundColor Green
 }
-Write-Host "tfenv installed and configured successfully`n" -ForegroundColor Green
+
 
 # Install nvm
-Write-Host "Installing NVM..." -ForegroundColor Cyan
-$Output = curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Error failed to install NVM: $Output"
-    $LASTEXITCODE = 0
-    exit 1
+if (-not(Test-Path "~/.nvm")) {
+    Write-Host "Installing NVM..." -ForegroundColor Cyan
+    $Output = curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error failed to install NVM: $Output"
+        $LASTEXITCODE = 0
+        exit 1
+    }
+    Write-Host "NVM installed successfully`n" -ForegroundColor Green
 }
-Write-Host "NVM installed successfully`n" -ForegroundColor Green
+
 
 # Setup chezmoi
-Write-Host "Installing chezmoi..." -ForegroundColor Cyan
-$Output = bash -c /mnt/c/vdi-wsl-setup/setup-chezmoi.sh
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Error installing chezmoi: $Output"
-    $LASTEXITCODE = 0
-    exit 1
+if (-not(Test-Path "~/.local/share/chezmoi")) {
+    Write-Host "Installing chezmoi..." -ForegroundColor Cyan
+    $Output = bash -c /mnt/c/vdi-wsl-setup/setup-chezmoi.sh
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error installing chezmoi: $Output"
+        $LASTEXITCODE = 0
+        exit 1
+    }
+    Write-Host "chezmoi installed successfully`n"
 }
-Write-Host "chezmoi installed successfully`n"
